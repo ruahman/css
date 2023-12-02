5bb2bf487246efedf97bea72836f3fad4ce6e2ac
Diego Vila <diego@velascommerce.com>
2023-12-02 09:09:34 -0400

    finished topography

--- css_tut/postcss.config.js
+++ css_tut/postcss.config.js
@@ -2,9 +2,11 @@ import autoprefixer from "autoprefixer";
 import postcssPresetEnv from "postcss-preset-env";
 import postcssMixins from "postcss-mixins";
 import postcssImport from "postcss-import";
+import parser from "postcss-comment"; // allow inline comment
 
 // setup the plugins you want to use
 export default {
+  parser: parser,
   plugins: [
     autoprefixer, // auto prefex vender css rules
     postcssPresetEnv({ stage: 2 }), // add polyfills for modern css features
